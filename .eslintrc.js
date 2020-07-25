7fe6b1a1ffc56173b489690350ff34cdfac66daa
Tony Tang <tonytang.dev@gmail.com>
2020-07-25 18:16:53 +1000

    Update .eslintrc to override linting itself

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,65 +1,67 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended'
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2019,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'curly': [
+      'error',
+      'multi-or-nest'
     ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 2019,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint'
+    'eqeqeq': 'error',
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 2 }
     ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'operator-linebreak': [
+      'error',
+      'before'
+    ],
+    'no-useless-return': 'error',
+    'prefer-destructuring': ['error', {
+      'object': true,
+      'array': true
+    }],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  },
+  'overrides': [{
+    'files': ['.eslintrc.js'],
     'rules': {
-        'curly': [
-            'error',
-            'multi-or-nest'
-        ],
-        'eqeqeq': 'error',
-        'indent': [
-            'error',
-            2,
-            { 'SwitchCase': 2 }
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'object-curly-spacing': [
-            'error',
-            'always'
-        ],
-        'operator-linebreak': [
-            'error',
-            'before'
-        ],
-        'no-useless-return': 'error',
-        'prefer-destructuring': ['error', {
-            'object': true,
-            'array': true
-        }],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    },
-    'overrides': [{
-        'files': ['**/*.ts'],
-        'rules': {
-            'no-unused-vars': ["off"]
-        }
-    }]
+      'no-undef': 'off',
+    }
+  }]
 };
